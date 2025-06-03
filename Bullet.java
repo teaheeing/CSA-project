e283504a61df7a37cb468b52567de5b4ea1d486b
teaheeing <seconds19012926@gmail.com>
2025-06-03 22:55:04 +0900

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -1,10 +1,14 @@
 public class Bullet extends Sprite {
-    private int speed = 5; // 속도 조정 (너무 빠르면 충돌 판정이 안 됨)
+    private int speed = 5; 
 
     public Bullet(int x, int y) {
         super(x, y, 2, 5, "bullet.png");
     }
 
-    public int getSpeed() { return speed; }
-    public void updateY(int y) { super.updateY(y); }
+    public int getSpeed() {
+        return speed;
+    }
+    public void updateY(int y) {
+        super.updateY(y);
+    }
 }
