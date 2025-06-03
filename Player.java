8e01e49d9f1d3ab8c6b665133ab6ec6f79f4e447
teaheeing <seconds19012926@gmail.com>
2025-06-03 22:53:33 +0900

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,12 +1,10 @@
-public class Player extends Sprite{
-  int hp = 10;
-      
-    public Player(int xPosition, int yPosition){
-        super(xPosition, yPosition, 40, "soldier.png");
+public class Player extends Sprite {
+    private int hp = 10;
+
+    public Player(int x, int y) {
+        super(x, y, 5, 8, "soldier.png");
     }
 
-    public int getHp() {
-      return hp;
-    }
-
+    public int getHp() { return hp; }
+    public void updateHp(int hp) { this.hp = hp; }
 }
