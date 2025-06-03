bdf44ee79ab09e0ae20eb763b407cb0530e6ad15
teaheeing <seconds19012926@gmail.com>
2025-06-03 22:54:39 +0900

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,28 +1,25 @@
-public class Alien extends Sprite{
-    
-    int width = 10;
-    int height = 10;
-    int hp = 10;
-    public Alien(int xPosition, int yPosition){
-        super(xPosition, yPosition, 40, "alien.png");
-    }
-    public int getHp() {
-      return hp;
-    }
-    public void updateHp(int hp) {
-      this.hp = hp;
-    }
-    public int getWidth() {
-      return width;
-    }
-    public int getHeight() {
-      return width;
+public class Alien extends Sprite {
+    private int hp;
+    private int speed;
+    private int level;
+
+    public Alien(int x, int y, int level) {
+        super(x, y, 5, 5, "alien.png");
+        this.hp = 1 + level / 2;
+        this.speed = 1 + level;
+        this.level = level;
     }
 
-    public boolean checkHit(int bulletX, int bulletY){
-        if (Math.abs(bulletX - super.getX()) < 30 && Math.abs(bulletY - super.getY()) < 30){
-            return true;
-        }
-        return false;
+    public int getHp() {
+        return hp;
+    }
+    public void updateHp(int hp) {
+        this.hp = hp;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void updateY(int y) {
+        super.updateY(y);
     }
 }
